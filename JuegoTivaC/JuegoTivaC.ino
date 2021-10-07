cc853407ebefc9d6562c180b372a9006cbc9716c
JoseLuisA-P <alv19392@uvg.edu.gt>
2021-10-07 09:04:17 -0600

    Pantallas del juego y delay
    adicion de las pantallas de carga del juego como base, sin animacion, adicion del delay sin interrupcion

--- JuegoTivaC/JuegoTivaC.ino
+++ JuegoTivaC/JuegoTivaC.ino
@@ -8,7 +8,11 @@ Personaje player1(20,30,30,120); //objeto del personaje 1
 Personaje player2(200,30,200,120); //objeto del personaje 2
 
 File lectura; //para leer un archivo de la SD
-uint8_t animacion;
+uint8_t animacion = 0; //mover las animaciones de los personajes
+uint8_t mensel = 0; //seleccionar el menu
+bool onete; //utilizado para activar los eventos de una sola vez
+unsigned long prevTime = 0;
+uint8_t intervalo;
 
 void setup() {
   Serial.begin(9600);
@@ -25,23 +29,42 @@ void setup() {
   LCD_Print("Almacenamiento",55,35,1,0x0000,0xf4f4);  //si lo esta, lo indica y corre con el inicio normalmente
   LCD_Print("encontrado",55,55,1,0x0000,0xf4f4);
   delay(1000);
-  
-  lectura = SD.open("Bitmap2.TXT",FILE_READ);
-  bitmapSD(lectura, 320, 240);
-  lectura.close();
-  delay(1000);
 
-  player1.init();
-  player2.init();
+  LCD_Clear(0x62AA);
+  
+  //player1.init();
+  //player2.init();
+  onete = 1; //evento de una vez inicializado
+  intervalo = 500; //primer intervalo, se indicara el cambio en las pantallas de intermedio
 }
   
 
 void loop() {
+  unsigned long actTime = millis();
+    if(actTime-prevTime > intervalo){ //generar delays sin detener la ejecucion del codigo
+      prevTime = actTime;
+      animacion++;
+    }
+    
+  switch(mensel){
+    case 0:
+      if(onete) { LCD_Print("Inicio del juego",30,50,2,0x0000,0x62AA); onete = 0;} //para imprimir una vez el texto de inicio
+      
+      if(animacion)LCD_Print("Presiona el boton para comenzar",30,150,1,0x0000,0x62AA);
+      else FillRect(30,150,250,30,0x62AA); //animacion del texto de presionar START para ir a seleccion de personajes
+      if(animacion > 1) animacion = 0; //dos estados para presionar, se modifica dependiendo la pantalla desplegada
+      break;
+    case 1:
+    
+      break;
+    }
+
+    /*
     player1.updateSp();
     player2.updateSp();
     delay(10);
     animacion ++;
     if(animacion > 1) animacion = 0;
     player1.pose = animacion;
-    player2.pose = animacion;
+    player2.pose = animacion;*/
 }
