110b09842814c063ec8eaec32b2c7e8bd9e87977
JoseLuisA-P <alv19392@uvg.edu.gt>
2021-10-21 18:36:32 -0600

    juego y control actualizado
    Juego enlazado con el control, funcionalidades minimas completadas

--- JuegoTivaC/JuegoTivaC.ino
+++ JuegoTivaC/JuegoTivaC.ino
@@ -79,7 +79,7 @@ void loop() {
         inMes = Serial3.read();
         }
 
-      if(inMes == '1')mensel = 1; //al presionar start cambia de pantalla
+      if(inMes == '0')mensel = 1; //al presionar start cambia de pantalla
       break;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  //pantalla de seleccion de personajes
@@ -96,14 +96,14 @@ void loop() {
         inMes = 0;
         }
         
-      if(Serial.available()){ //leer el dato enviado por el control
-        inMes = Serial.read();
+      if(Serial3.available()){ //leer el dato enviado por el control
+        inMes = Serial3.read();
         switch(inMes){ //dependiendo del valor seleccionado, asi cambia la seleccion del personaje
-          case '2':
+          case '1':
             player1.skinsel ++;
             if(player1.skinsel>1)player1.skinsel = 0; //solo dos estados de cambio
             break;
-          case '3':
+          case '4':
             player2.skinsel ++;
             if(player2.skinsel >1)player2.skinsel = 0; //solo dos estados
             break;
@@ -112,7 +112,7 @@ void loop() {
           
         }
 
-        if (inMes == '9'){
+        if (inMes == '0'){
           mensel = 2;
           LCD_Clear(0x0000);
           lectura = SD.open("FONPEL.TXT", FILE_READ); //FONDO DE LA PELEA
@@ -156,8 +156,8 @@ void loop() {
       if(skip2 == 0)player2.pose = animacion;
       else player2.pose = 1; //carga del ataque
       if(animacion > 1) animacion = 0; //no pasarse de las posiciones de los personajes
-      if(Serial.available()){ //leer el dato enviado por el control
-        inMes = Serial.read();
+      if(Serial3.available()){ //leer el dato enviado por el control
+        inMes = Serial3.read();
         }
       if(!turno) LCD_Print("Turno P1",100,82,2,0x0000,0xFFFF); //indicar el turno de los personajes
       else LCD_Print("Turno P2",100,82,2,0x0000,0xFFFF);
@@ -286,6 +286,7 @@ void loop() {
       if((2 + player2.skinsel) == 2)lectura = SD.open("ROJO1.TXT", FILE_READ);
       spriteSD(lectura,150,100);
       lectura.close();
+      Serial3.write(66);
       break;
     case 4:
       LCD_Clear(0x0000);
@@ -297,6 +298,7 @@ void loop() {
       if((player1.skinsel) == 0)lectura = SD.open("AZUL1.TXT", FILE_READ);
       spriteSD(lectura,150,100);
       lectura.close();
+      Serial3.write(65);
       break;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  //pantalla de reinicio y para reiniciar los parametros de los jugadores
@@ -305,11 +307,11 @@ void loop() {
       if(animacion)LCD_Print("Presiona start volver",80,150,1,0xFFFF,0x0000);
       else FillRect(30,150,250,30,0x0000); //animacion del texto de presionar START para ir a seleccion de personajes
       if(animacion > 1) animacion = 0;
-      if(Serial.available()) inMes = Serial.read();
+      if(Serial3.available()) inMes = Serial3.read();
 
       updateWinner(animacion);
       
-      if(inMes == '1'){
+      if(inMes == '0'){
         mensel = 0;
         LCD_Clear(0x0000);
         player1.health = 100;
